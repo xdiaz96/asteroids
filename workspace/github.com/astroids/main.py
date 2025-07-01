107c1665d9a501a2d03fc620dfd4649be2823d1c
xdiaz96 <xadiaz996@gmail.com>
2025-07-01 16:50:01 -0700

    Init files and black screen working

---
+++ workspace/github.com/astroids/main.py
@@ -0,0 +1,22 @@
+# this allows us to use code from
+# the open-source pygame library
+# throughout this file
+import pygame
+from constants import *
+
+def main():
+    pygame.init()
+    print("Starting Asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    while(True):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        pygame.Surface.fill(screen, "black")
+        pygame.display.flip()
+
+
+if __name__ == "__main__":
+    main()
